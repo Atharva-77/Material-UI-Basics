91c1547969830cd2c2a1ee2ff6ff7c609f095cd9
unknown <atharva.shirode99@gmail.com>
2022-10-06 11:52:24 +0530

    Typography added

--- material-ui-app/src/App.js
+++ material-ui-app/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MultTypography from './MultTypography';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       hi
-      </header>
+     hh
+     <MultTypography/>
     </div>
   );
 }
