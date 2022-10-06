705fadb5b570fd80a7d0b05264707e59c37221bd
unknown <atharva.shirode99@gmail.com>
2022-10-06 17:51:07 +0530

    CreateTheme, ThemeProvider added

--- material-ui-app/src/App.js
+++ material-ui-app/src/App.js
@@ -3,15 +3,43 @@ import './App.css';
 import MultTypography from './MultTypography';
 import ButtonUI from './ButtonUI';
 import React from 'react';
+import { MuiThemeProvider, createTheme, makeStyles ,withStyles} from '@material-ui/core/styles';
+import createPalette from '@mui/material/styles/createPalette';
+// import { withStyles } from '@material-ui/core/styles';
+
+import { ThemeProvider } from '@mui/material';
+
+const theme=createTheme({
+    palette: {
+        primary: {
+            main: '#34ebc3'
+        },
+        secondary:{
+          main:'#fc1212'
+        },
+
+    //     typography:// No chnage. The import added in index.css changes the font.
+    //     {
+    //       fontFamily:'Roboto'
+    //     }
+    }
+})
 
 function App() {
   return (
-    <div className="App">
-     Hello App.jsh
-     <MultTypography/>
-     {/* <ButtonUI /> */}
-    </div>
+    <ThemeProvider theme={theme} >
+        {console.log("\ 1.THEME",theme)} 
+       
+        <div className="App">
+             Hello App.jsh
+             <MultTypography/>
+              <ButtonUI />
+        
+        </div>
+
+     </ThemeProvider>
   );
 }
 
 export default App;
+// export default withStyles({ withTheme: true })(App);
--- material-ui-app/src/MultTypography.js
+++ material-ui-app/src/MultTypography.js
@@ -2,30 +2,59 @@ import React from 'react';
 // import {Typography} from '@mui/material'
 import Typography from '@mui/material/Typography'
 import { Container } from '@mui/system';//Margin to left & right
-import { makeStyles } from '@mui/styles';
+// import { makeStyles, createTheme,ThemeProvider } from '@mui/styles';
+// import { createTheme } from '@material-ui/core';
+import { ThemeProvider, createTheme, makeStyles } from '@material-ui/core/styles';
+// import { ThemeProvider, createTheme, makeStyles } from '@mui/material/styles';
+// import { palette} from '@mui/system';
 
-const useStyle=makeStyles({
-    btn:{
-        fontSize:60,
-        backgroundColor:'yellow',
-        color: 'purple',
-        '&:hover':
-        {
-            backgroundColor:'blue'
-        }
-    }
-})
+// const useStyle=makeStyles({
+//     btn:{
+//         fontSize:60,
+//         backgroundColor:'yellow',
+//         color: 'purple',
+//         '&:hover':
+//         {
+//             backgroundColor:'blue'
+//         }
+//     }
+// })
+// --
+// const theme = createTheme({
+//   p1alette: {
+//     primary: {
+//       main: "white",
+//     },
+//     secondary: {
+//       main: "green",
+//     },
+//   },
+// });
+// --
+
+// const theme1=createTheme({
+//     palette: {
+//         primary: {
+//             main: "#fc1212"
+//         }
+//     }
+// })
+// const theme={};
 
 function MultTypography() {
 
-    const classes=useStyle();
+    // const classes=useStyle();
+    // console.log("CLASSES","\nTHEME",theme1);
     
     return (
         <Container>
-             In MultiTypography
+             {/* <ThemeProvider theme={theme1}> */}
+
+            
+             {/* In MultiTypography */}
 
             <Typography 
-                className={classes.btn}
+                // className={classes.btn}
                 variant="h1"
                 // color="primary"
                 align="left"
@@ -36,7 +65,7 @@ function MultTypography() {
 
              <Typography 
                 variant="h1"
-                color="error"
+                color="primary"
                 align="center">
                  h11. Heading
             </Typography>
@@ -44,14 +73,14 @@ function MultTypography() {
             <Typography 
                 variant="h6"
                 component="h2"  // The size is h6, but on inspecting, the tag is enclosed in h2.
-                color="textsecondary"
+                color="secondary"
                 gutterBottom //Adds margin to bottom. Yellow line at bottom on insepcting.
                 >
                 Creating a new App
             </Typography>
 
-
-        </Container>
+          {/* </ThemeProvider> */}
+         </Container>
     );
 }
 
